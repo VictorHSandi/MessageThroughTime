2db959eb2826ac33f77d50f821856c94288faff9
emmas0813 <emma.k.smith-1@ou.edu>
2024-02-10 15:37:45 -0600

    updated html and css file

--- client-messagethroughtime/src/router/index.js
+++ client-messagethroughtime/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { HomePage } from "../views/HomePage.vue";
+import HomePage from "../views/HomePage.vue";
 
 const router = createRouter({
     history: createWebHistory(),
