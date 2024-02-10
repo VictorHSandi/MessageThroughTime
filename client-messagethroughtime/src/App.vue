987c5e7c6c171948536f2feffa2c5371c8b1c797
emmas0813 <emma.k.smith-1@ou.edu>
2024-02-10 16:10:09 -0600

    updated homepage

--- client-messagethroughtime/src/App.vue
+++ client-messagethroughtime/src/App.vue
@@ -17,11 +17,7 @@ export default {
 <style>
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  text-decoration-line: underline;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
