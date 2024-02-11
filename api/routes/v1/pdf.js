53ba98d907936453fcbb856f4d2382ed4e98ee76
Preston Roesslet <proesslet@gmail.com>
2024-02-11 02:11:08 -0600

    generate pdf endpoint done

--- api/routes/v1/pdf.js
+++ api/routes/v1/pdf.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-// generate pdf 
+// generate pdf
 router.get("/", (req, res) => {
   res.send("This is the PDF API");
 });
 
-
-
+router.post("/generate", (req, res) => {
+  const message = req.body.message;
+  // Use the pdfgenerator in services directory
+  const pdfGenerator = require("../../services/pdfgenerator");
+  pdfGenerator.takeInput(message);
+  setTimeout(() => {
+    res
+      .status(200)
+      .sendFile("./services/output.pdf", { root: __dirname + "/../../" });
+  }, 2000);
+});
 
 module.exports = router;
