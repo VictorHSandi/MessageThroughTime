ea4d2ee3a8dd56e00efd842543c59105e1bc5cec
Preston Roesslet <proesslet@gmail.com>
2024-02-11 05:35:06 -0600

    started connecting frontend

--- client-messagethroughtime/src/main.js
+++ client-messagethroughtime/src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 
 const app = createApp(App);
+axios.defaults.baseURL = "https://api.messagethroughtime.tech/api/v1/";
+axios.defaults.withCredentials = true;
 app.use(router);
-app.mount('#app')
+app.mount("#app");
