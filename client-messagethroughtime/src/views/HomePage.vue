f61b02def559ed2f03313f7443bd99cc77d3dc7d
KPeters <99045807+kpe03@user.noreply.github.com>
2024-02-10 18:18:43 -0600

    added terminal input

--- client-messagethroughtime/src/views/HomePage.vue
+++ client-messagethroughtime/src/views/HomePage.vue
@@ -3,11 +3,26 @@
         <p>
             This is the home page!!
         </p>
+        <div class="messageinput">
+            <MessageInput v-model="inputValue" type="text" label="Encrypt Message" />
+        </div>
+       
     </div>
 </template>
 
 <script>
+import MessageInput from "@/components/MessageInput.vue";
+
 export default {
     name: "HomePage",
+    components: {
+        MessageInput
+    },
+    data() {
+        return {
+            inputValue: "" //data to bind to input
+        }
+    }
 };
 </script>
+
