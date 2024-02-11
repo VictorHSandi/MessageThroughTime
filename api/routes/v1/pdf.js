03fbb68f1b07d08311a0fd0eabf4cb81d4e6ec69
emmas0813 <emma.k.smith-1@ou.edu>
2024-02-11 01:16:19 -0600

    updated index.js to convert binary to char

--- api/routes/v1/pdf.js
+++ api/routes/v1/pdf.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+// generate pdf 
 router.get("/", (req, res) => {
   res.send("This is the PDF API");
 });
+
+
+
 
 module.exports = router;
