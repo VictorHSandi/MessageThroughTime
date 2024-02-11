3dd977557d992b5cff8fc69b1c3266d442c13202
KPeters <99045807+kpe03@user.noreply.github.com>
2024-02-10 19:28:29 -0600

    fixed

--- client-messagethroughtime/src/views/HomePage.vue
+++ client-messagethroughtime/src/views/HomePage.vue
@@ -1,32 +1,4 @@
 <template>
-    <div>
-        <p>
-            This is the home page!!
-        </p>
-        <div class="messageinput">
-            <MessageInput v-model="inputValue" type="text" label="Encrypt Message" />
-        </div>
-       
-    </div>
-</template>
-
-<script>
-import MessageInput from "@/components/MessageInput.vue";
-
-export default {
-    name: "HomePage",
-    components: {
-        MessageInput
-    },
-    data() {
-        return {
-            inputValue: "" //data to bind to input
-        }
-    }
-};
-</script>
-
-<template>
     <div class ="title">
         <h1>Welcome to Message through time</h1>
     </div>
@@ -34,6 +6,10 @@ export default {
     <div class="page info">
         <p>Add info here</p>
     </div>
+    <div class="messageinput">
+            <MessageInput v-model="inputValue" type="text" label="Encrypt Message" />
+        </div>
+       
 </template>
 
 <script>
